a97c0b84b8c929ee93a920370dea2e158ab6d31b
Valiant <c14220157@john.petra.ac.id>
2024-11-05 19:01:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanlistview/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.latihanlistview
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // data initialization
+        var data = mutableListOf<String>()
+        data.addAll(listOf("1", "2", "3", "4", "5"))
+
+        val lvAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            data
+        )
+
+        val _lv1 = findViewById<ListView>(R.id.lv1)
+        _lv1.adapter = lvAdapter
+
+        // add button
+        val _btnTambah = findViewById<Button>(R.id.btnTambah)
+        _btnTambah.setOnClickListener {
+            var dtAkhir = Integer.parseInt(data.get(data.size - 1)) + 1
+            data.add(dtAkhir.toString())
+            lvAdapter.notifyDataSetChanged()
+        }
+
+        // toast
+        _lv1.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(
+                this, "${data[position]}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        // delete button
+        val _btnHapus = findViewById<Button>(R.id.btnHapus)
+        _btnHapus.setOnClickListener {
+            if (data.size <= 0) {
+                Toast.makeText(
+                    this, "Data kosong",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                data.removeFirst()
+                lvAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+}
